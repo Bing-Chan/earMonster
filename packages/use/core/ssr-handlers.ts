d5b53b7ea7440713e5a1829deac20ec43c05653b
02359935 <02359935@yto.net.cn>
2023-06-19 17:58:17 +0800

    处理页面变化

---
+++ packages/use/core/ssr-handlers.ts
@@ -0,0 +1,56 @@
+/* eslint-disable no-restricted-globals */
+import type { Awaitable } from '@vueuse/shared'
+import type { MaybeElementRef } from './unrefElement'
+
+export interface StorageLikeAsync {
+  getItem(key: string): Awaitable<string | null>
+  setItem(key: string, value: string): Awaitable<void>
+  removeItem(key: string): Awaitable<void>
+}
+
+export interface StorageLike {
+  getItem(key: string): string | null
+  setItem(key: string, value: string): void
+  removeItem(key: string): void
+}
+
+/**
+ * @experimental The API is not finalized yet. It might not follow semver.
+ */
+export interface SSRHandlersMap {
+  getDefaultStorage: () => StorageLike | undefined
+  getDefaultStorageAsync: () => StorageLikeAsync | undefined
+  updateHTMLAttrs: (selector: string | MaybeElementRef, attribute: string, value: string) => void
+}
+
+const _global
+  = typeof globalThis !== 'undefined'
+    ? globalThis
+    : typeof window !== 'undefined'
+      ? window
+      : typeof global !== 'undefined'
+        ? global
+        : typeof self !== 'undefined'
+          ? self
+          : {}
+
+const globalKey = '__vueuse_ssr_handlers__'
+const handlers = /*#__PURE__*/ getHandlers()
+
+function getHandlers() {
+  if (!(globalKey in _global))
+    // @ts-expect-error inject global
+    _global[globalKey] = _global[globalKey] || {}
+  // @ts-expect-error inject global
+  return _global[globalKey] as Partial<SSRHandlersMap>
+}
+
+export function getSSRHandler<T extends keyof SSRHandlersMap>(key: T, fallback: SSRHandlersMap[T]): SSRHandlersMap[T]
+export function getSSRHandler<T extends keyof SSRHandlersMap>(key: T, fallback: SSRHandlersMap[T] | undefined): SSRHandlersMap[T] | undefined
+export function getSSRHandler<T extends keyof SSRHandlersMap>(key: T, fallback?: SSRHandlersMap[T]): SSRHandlersMap[T] | undefined {
+  return handlers[key] as SSRHandlersMap[T] || fallback
+}
+
+export function setSSRHandler<T extends keyof SSRHandlersMap>(key: T, fn: SSRHandlersMap[T]) {
+  handlers[key] = fn
+}
---
+++ packages/use/core/useEventListener/index.ts
@@ -0,0 +1,165 @@
+import type { Arrayable, Fn, MaybeRefOrGetter } from '@vueuse/shared'
+import { noop, toValue, tryOnScopeDispose } from '@vueuse/shared'
+import { watch } from 'vue-demi'
+import type { MaybeElementRef } from '../unrefElement'
+import { unrefElement } from '../unrefElement'
+import { defaultWindow } from '../_configurable'
+
+interface InferEventTarget<Events> {
+  addEventListener(event: Events, fn?: any, options?: any): any
+  removeEventListener(event: Events, fn?: any, options?: any): any
+}
+
+export type WindowEventName = keyof WindowEventMap
+export type DocumentEventName = keyof DocumentEventMap
+
+export interface GeneralEventListener<E = Event> {
+  (evt: E): void
+}
+
+/**
+ * Register using addEventListener on mounted, and removeEventListener automatically on unmounted.
+ *
+ * Overload 1: Omitted Window target
+ *
+ * @see https://vueuse.org/useEventListener
+ * @param event
+ * @param listener
+ * @param options
+ */
+export function useEventListener<E extends keyof WindowEventMap>(
+  event: Arrayable<E>,
+  listener: Arrayable<(this: Window, ev: WindowEventMap[E]) => any>,
+  options?: MaybeRefOrGetter<boolean | AddEventListenerOptions>
+): Fn
+
+/**
+ * Register using addEventListener on mounted, and removeEventListener automatically on unmounted.
+ *
+ * Overload 2: Explicitly Window target
+ *
+ * @see https://vueuse.org/useEventListener
+ * @param target
+ * @param event
+ * @param listener
+ * @param options
+ */
+export function useEventListener<E extends keyof WindowEventMap>(
+  target: Window,
+  event: Arrayable<E>,
+  listener: Arrayable<(this: Window, ev: WindowEventMap[E]) => any>,
+  options?: MaybeRefOrGetter<boolean | AddEventListenerOptions>
+): Fn
+
+/**
+ * Register using addEventListener on mounted, and removeEventListener automatically on unmounted.
+ *
+ * Overload 3: Explicitly Document target
+ *
+ * @see https://vueuse.org/useEventListener
+ * @param target
+ * @param event
+ * @param listener
+ * @param options
+ */
+export function useEventListener<E extends keyof DocumentEventMap>(
+  target: DocumentOrShadowRoot,
+  event: Arrayable<E>,
+  listener: Arrayable<(this: Document, ev: DocumentEventMap[E]) => any>,
+  options?: MaybeRefOrGetter<boolean | AddEventListenerOptions>
+): Fn
+
+/**
+ * Register using addEventListener on mounted, and removeEventListener automatically on unmounted.
+ *
+ * Overload 4: Custom event target with event type infer
+ *
+ * @see https://vueuse.org/useEventListener
+ * @param target
+ * @param event
+ * @param listener
+ * @param options
+ */
+export function useEventListener<Names extends string, EventType = Event>(
+  target: InferEventTarget<Names>,
+  event: Arrayable<Names>,
+  listener: Arrayable<GeneralEventListener<EventType>>,
+  options?: MaybeRefOrGetter<boolean | AddEventListenerOptions>
+): Fn
+
+/**
+ * Register using addEventListener on mounted, and removeEventListener automatically on unmounted.
+ *
+ * Overload 5: Custom event target fallback
+ *
+ * @see https://vueuse.org/useEventListener
+ * @param target
+ * @param event
+ * @param listener
+ * @param options
+ */
+export function useEventListener<EventType = Event>(
+  target: MaybeRefOrGetter<EventTarget | null | undefined>,
+  event: Arrayable<string>,
+  listener: Arrayable<GeneralEventListener<EventType>>,
+  options?: MaybeRefOrGetter<boolean | AddEventListenerOptions>
+): Fn
+
+export function useEventListener(...args: any[]) {
+  let target: MaybeRefOrGetter<EventTarget> | undefined
+  let events: Arrayable<string>
+  let listeners: Arrayable<Function>
+  let options: MaybeRefOrGetter<boolean | AddEventListenerOptions> | undefined
+
+  if (typeof args[0] === 'string' || Array.isArray(args[0])) {
+    [events, listeners, options] = args
+    target = defaultWindow
+  }
+  else {
+    [target, events, listeners, options] = args
+  }
+
+  if (!target)
+    return noop
+
+  if (!Array.isArray(events))
+    events = [events]
+  if (!Array.isArray(listeners))
+    listeners = [listeners]
+
+  const cleanups: Function[] = []
+  const cleanup = () => {
+    cleanups.forEach(fn => fn())
+    cleanups.length = 0
+  }
+
+  const register = (el: any, event: string, listener: any, options: any) => {
+    el.addEventListener(event, listener, options)
+    return () => el.removeEventListener(event, listener, options)
+  }
+
+  const stopWatch = watch(
+    () => [unrefElement(target as unknown as MaybeElementRef), toValue(options)],
+    ([el, options]) => {
+      cleanup()
+      if (!el)
+        return
+
+      cleanups.push(
+        ...(events as string[]).flatMap((event) => {
+          return (listeners as Function[]).map(listener => register(el, event, listener, options))
+        }),
+      )
+    },
+    { immediate: true, flush: 'post' },
+  )
+
+  const stop = () => {
+    stopWatch()
+    cleanup()
+  }
+
+  tryOnScopeDispose(stop)
+
+  return stop
+}
