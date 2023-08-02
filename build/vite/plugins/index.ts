2654a78902ddd73b59cd219c41b7b3740484ead8
duochan <02359935@yto.net.cn>
2023-08-02 15:43:14 +0800

    vite 插件处理

--- build/vite/plugins/index.ts
+++ build/vite/plugins/index.ts
@@ -6,28 +6,16 @@
 import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { ConfigSvgIconsPlugin } from './svgIcons';
-import { AutoRegistryComponents } from './component';
 import { ConfigCompressPlugin } from './compress';
 import { ConfigRestartPlugin } from './restart';
 import { ConfigProgressPlugin } from './progress';
 
-
 export function createVitePlugins(env: ViteEnv, isBuild: boolean) {
-  const { VITE_USE_MOCK, VITE_USE_ERUDA } = env;
-
   const vitePlugins: (PluginOption | PluginOption[])[] = [
     // vue支持
     vue(),
   ];
 
-  // 自动按需引入组件
-  vitePlugins.push(AutoRegistryComponents());
-
-  // 自动按需引入依赖
-  vitePlugins.push(AutoImportDeps());
-
-  // 自动生成路由
-  vitePlugins.push(ConfigPagesPlugin());
 
   // 开启.gz压缩  rollup-plugin-gzip
   vitePlugins.push(ConfigCompressPlugin());
