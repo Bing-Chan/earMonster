a3d036f5768fbb07eb8218026a06ab4bb2cc8f7d
duochan <346632482@qq.com>
2023-08-03 20:13:21 +0800

    上传静态包

---
+++ docs/.vitepress/dist/assets/component_pc_index.md.ef6bb072.lean.js
@@ -0,0 +1,21 @@
+import{_ as n,W as s,Z as a,aD as t}from"./plugin-vue_export-helper.1f080e1f.js";const _='{"title":"\u{1F477}\u5B89\u88C5","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u{1F477}\u5B89\u88C5","slug":"\u{1F477}\u5B89\u88C5"},{"level":2,"title":"\u{1F3CC}\u5FEB\u901F\u4E0A\u624B","slug":"\u{1F3CC}\u5FEB\u901F\u4E0A\u624B"}],"relativePath":"component/pc/index.md","lastUpdated":1691064709583}',e={},p=t(`__VP_STATIC_START__<h2 id="\u{1F477}\u5B89\u88C5" tabindex="-1">\u{1F477}\u5B89\u88C5 <a class="header-anchor" href="#\u{1F477}\u5B89\u88C5" aria-hidden="true">#</a></h2><div class="language-shell"><pre><code><span class="token comment"># Vue 3 \u9879\u76EE\uFF0C\u5B89\u88C5\u6700\u65B0\u7248  @ear-monster/components</span>
+<span class="token function">npm</span> i  @ear-monster/components
+</code></pre></div><p>\u5F53\u7136\uFF0C\u4F60\u4E5F\u53EF\u4EE5\u901A\u8FC7 <code>yarn</code> \u6216 <code>pnpm</code> \u8FDB\u884C\u5B89\u88C5\uFF1A</p><div class="language-bash"><pre><code><span class="token comment"># \u901A\u8FC7 yarn \u5B89\u88C5</span>
+<span class="token function">yarn</span> <span class="token function">add</span>  @ear-monster/components
+
+<span class="token comment"># \u901A\u8FC7 pnpm \u5B89\u88C5</span>
+<span class="token function">pnpm</span> <span class="token function">add</span>  @ear-monster/components
+</code></pre></div><h2 id="\u{1F3CC}\u5FEB\u901F\u4E0A\u624B" tabindex="-1">\u{1F3CC}\u5FEB\u901F\u4E0A\u624B <a class="header-anchor" href="#\u{1F3CC}\u5FEB\u901F\u4E0A\u624B" aria-hidden="true">#</a></h2><div class="language-js"><pre><code><span class="token keyword">import</span> <span class="token punctuation">{</span> createApp <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;vue&#39;</span><span class="token punctuation">;</span>
+<span class="token comment">//\u{1F602}\u{1F602}\u{1F602}\u7531\u4E8E\u81EA\u5DF1\u5F53\u65F6\u7533\u8BF7\u7684npm\u5305\u8D77\u4E86\u8FD9\u4E2A\u540D\u5B57\uFF0C\u73B0\u5728\u6CA1\u6CD5\u6539\u4E86\uFF0C\u91CD\u65B0\u7533\u8BF7\u9700\u8981\u4EA4\u94B1\uFF0C\u6211\u5C31\u653E\u5F03\u4E86\uFF0C\u5148\u5C06\u5C31\u7528\u7740</span>
+<span class="token keyword">import</span> <span class="token operator">*</span> <span class="token keyword">as</span> galaxyUI <span class="token keyword">from</span> <span class="token string">&#39; @ear-monster/components&#39;</span><span class="token punctuation">;</span>
+
+<span class="token keyword">const</span> app <span class="token operator">=</span> <span class="token function">createApp</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token comment">//\u6CE8\u518C\u4F60\u7684\u5168\u5C40\u7EC4\u4EF6</span>
+app<span class="token punctuation">.</span><span class="token function">use</span><span class="token punctuation">(</span>earMonster<span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// \u4E5F\u53EF\u4EE5\u6309\u9700\u5F15\u5165\u54E6~</span>
+<span class="token keyword">import</span> <span class="token punctuation">{</span>EarIntroStep  <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39; @ear-monster/components&#39;</span><span class="token punctuation">;</span>
+
+<span class="token comment">//\u6CE8\u518C\u4F60\u7684\u5168\u5C40\u7EC4\u4EF6</span>
+app<span class="token punctuation">.</span><span class="token function">compoent</span><span class="token punctuation">(</span>EarIntroStep<span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre></div>__VP_STATIC_END__`,6),o=[p];function c(r,l,i,u,k,d){return s(),a("div",null,o)}var f=n(e,[["render",c]]);export{_ as __pageData,f as default};
