2dac824848538c0cbca37b9dca4388be890acf4c
duochan <346632482@qq.com>
2023-08-03 14:36:08 +0800

    处理测试代码

--- demo/App.vue
+++ demo/App.vue
@@ -1,26 +1,21 @@
 <template>
 	<div>
-		<div style="width: 500px;height: 300px;">
-			<ear-sign width="500"></ear-sign>
-		</div>
-
+		<el-card v-for="(item, index) in moduleList" :key="index" class="box-card">
+			<template #header>
+				<div class="card-header">
+					<span>Card name</span>
+					<el-button class="button" text>Operation button</el-button>
+				</div>
+			</template>
+			<div>
+				<component :is="item"></component>
+			</div>
+		</el-card>
 	</div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { defineComponent, reactive, toRefs } from 'vue'
 
-export default defineComponent({
-	name: 'App',
-	setup() {
-		const state = reactive({
-			include: ['perProgram', 'sectionPrice'],
-			values: [],
-		})
-
-		return {
-			...toRefs(state),
-		}
-	},
-})
+const moduleList = []
 </script>
